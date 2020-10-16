bb0783866ee9a4519bdea2325742f82ebd630667
lakhangaikwad8488 <70312128+lakhangaikwad8488@users.noreply.github.com>
2020-10-16 08:55:59 +0530

    Add files via upload

---
+++ Area Volume Calculator/src/main/java/main/java/yaksha/Sphere.java
@@ -0,0 +1,32 @@
+package main.java.yaksha;
+
+public class Sphere extends Shape implements Spatial {
+	
+	private double radius;
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		/* 
+ 		return 4*Math.PI*radius;
+ 		*/
+		return 0;
+	}
+
+	@Override
+	public double volume() {
+		/*
+		return ( 4  * Math.PI * radius ) / 3;
+		*/
+		return 0;
+	}
+	
+
+}
